507495f234e1c4c92bc0e70eb3bb4d0ad6946492
jhyeon000 <84574269+jhyeon000@users.noreply.github.com>
2021-06-05 02:30:45 +0900

    ReStatus

---
+++ survivalGame/Assets/Scripts/UIscripts/youarestupid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class youarestupid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //e bao mungchung a !
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
